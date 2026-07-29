eba0ddddd54d66ad5c4fc3579a24cfa8a459e843
iammdtanvirrahman <mdtanvirrahman2022a@gmail.com>
2026-07-29 23:25:11 +0600

    Create simulation.js

---
+++ js/js/simulation.js
@@ -0,0 +1,6 @@
+export function createSimulation(root) {
+    root.innerHTML = `
+        <h2>🌌 Simulation Area</h2>
+        <p>Launch, landing, and space travel features will be added here.</p>
+    `;
+}
