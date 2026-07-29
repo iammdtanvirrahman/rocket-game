f0cba01d0f86213de7fdfd33f0350b5776ee230a
iammdtanvirrahman <mdtanvirrahman2022a@gmail.com>
2026-07-29 23:48:42 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-import { createBuilder } from './builder.js';
+import { createBuilder, getRocketStats } from './builder.js';
 import { createSimulation } from './simulation.js';
 
 const builderRoot = document.getElementById('builder-root');
 const simulationRoot = document.getElementById('simulation-root');
 
-createBuilder(builderRoot);
-createSimulation(simulationRoot);
+createBuilder(builderRoot, (stats) => {
+    createSimulation(simulationRoot, stats);
+});
+
+createSimulation(simulationRoot, getRocketStats());
